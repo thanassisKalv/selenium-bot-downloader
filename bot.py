3bc90c25d27a518901012de60ca38a89fb287f3d
thanassisKalv <thanassiskalv@gmail.com>
2022-09-26 17:48:53 +0300

    Update bot.py
    - added optional support for usage of proxies with selenium
    - main loop check if proxy is blocked and displays an alert message

--- bot.py
+++ bot.py
@@ -8,6 +8,10 @@ from selenium.webdriver.chrome.options import Options
 import time
 import base64
 import json
+import requests
+from bs4 import BeautifulSoup
+ 
+USE_PROXY = True
 
 LOCAL_FOLDER = "./local_folder/"
 
@@ -16,15 +20,38 @@ target_URLs = ['https://archive.ics.uci.edu/ml/machine-learning-databases/liver-
                 "https://archive.ics.uci.edu/ml/machine-learning-databases/ipums-mld/"]
 
 chromeOptions = selenium.webdriver.ChromeOptions()
-#prefs = {"download.default_directory" : "./test"}
-#chromeOptions.add_experimental_option("prefs",prefs)
 
-chromeOptions.add_argument("--disable-extensions")
-chromeOptions.add_argument("--disable-gpu")
-chromeOptions.add_argument("--headless")
 
-print('Selenium opens headless browser...')
-driver = selenium.webdriver.Chrome(options=chromeOptions) 
+def filter_proxies():   
+    response = requests.get('https://www.sslproxies.org/')
+    soup = BeautifulSoup(response.text,"html.parser")
+    proxies = []
+    for item in soup.select("table.table tbody tr"):
+        if not item.select_one("td"):break
+        ip = item.select_one("td").text
+        port = item.select_one("td:nth-of-type(2)").text
+        proxies.append(f"{ip}:{port}")
+    return proxies
+
+# possible solutions for getting a list of PROXIES --> https://gist.github.com/tushortz/cba8b25f9d80f584f807b65890f37be5
+def create_proxy_driver(PROXY):
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    if USE_PROXY:
+        options.add_argument(f'--proxy-server={PROXY}')
+    driver = selenium.webdriver.Chrome(options=options)
+    return driver
+
+if USE_PROXY:
+    print('Selenium opens headless browser... (with Proxy)')
+else:
+    print('Selenium opens headless browser...')
+# creating new driver to use proxy
+ALL_PROXIES = filter_proxies()
+proxydriver = create_proxy_driver(ALL_PROXIES)
+
+#proxydriver = selenium.webdriver.Chrome(options=chromeOptions) 
 
 def save_file(download_url):
 
@@ -48,13 +75,19 @@ def load_downloaded():
     fp.close()
     return urls
 
+
 for target_url in target_URLs:
-    driver.get( target_url )
-    print("Checking directory:", target_url)
-    time.sleep(1)
+    proxydriver.get( target_url )
+    print("Checking directory with url:", target_url)
+    time.sleep(3)
+
+    if "<html><head></head><body></body></html>" in proxydriver.page_source or \
+         "ERR_NO_SUPPORTED_PROXIES" in proxydriver.page_source:
+        print("PROBLEM: PROXIES ARE NOT SUPPORTED IN THIS PAGE!")
+        continue
 
     scraped_URLs = []
-    elems = driver.find_elements(By.XPATH, "//a[@href]")
+    elems = proxydriver.find_elements(By.XPATH, "//a[@href]")
     for elem in elems:
         #print(elem.get_attribute("href"))
         scraped_URLs.append(elem.get_attribute("href"))
@@ -67,7 +100,7 @@ for target_url in target_URLs:
             continue
 
         print('Injecting retrieval code into web page')
-        driver.execute_script("""
+        proxydriver.execute_script("""
             window.file_contents = null;
             var xhr = new XMLHttpRequest();
             xhr.responseType = 'blob';
@@ -88,7 +121,7 @@ for target_url in target_URLs:
         downloaded_file = None
         while downloaded_file is None:
             # Returns the file retrieved base64 encoded (perfect for downloading binary)
-            downloaded_file = driver.execute_script('return (window.file_contents !== null ? window.file_contents.split(\',\')[1] : null);')
+            downloaded_file = proxydriver.execute_script('return (window.file_contents !== null ? window.file_contents.split(\',\')[1] : null);')
             #print(downloaded_file)
             if not downloaded_file:
                 print('\tNot downloaded, waiting...')
@@ -101,4 +134,4 @@ for target_url in target_URLs:
 
         print('\tDone')
 
-driver.close() # close web browser before exit
+proxydriver.close() # close web browser before exit
